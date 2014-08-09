e433476258c3cae0cfc28169e6c95102ae925534
Jose Contreras <jocontre@joselcontreras.com>
2014-08-09 01:56:29 -0700

    Testing mongo data to view

--- server.js
+++ server.js
@@ -35,13 +35,24 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Oh noe, connection error...'));
 db.once('open', function callback(){ // Log this message once
 	console.log('Success! App db opened');
-})
+});
+
+var messageSchema = mongoose.Schema({message: String}); // Set up message scheme in Mongoose
+var Message = mongoose.model('Message', messageSchema); // Create a model based on the schema (similar to how Angular does it)
+var mongoMessage;
+
+Message.findOne().exec(function(err, messageDoc){
+	mongoMessage = messageDoc.message;
+});
+
 app.get('/partials/:partialPath', function(req, res) {
 	res.render('partials/' + req.params.partialPath);
 });
 app.get('*', function (req, res) {
 	// * will match all routes (any req that gets to this point will be handled; JS CSS and image requests)
-	res.render('index');
+	res.render('index', {
+		mongoMessage: mongoMessage // Give mongodb mongoMessage to variable to be used in jade file to display to view. 
+	});
 })
 
 // Tell app to listen for ports
